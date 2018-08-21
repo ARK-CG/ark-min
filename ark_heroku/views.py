35632584b17f438f1c702b9dab9a21520f31acf0
ErgoFriend <ergoproxy2236@yahoo.co.jp>
2018-08-21 16:24:17 +0900

    html

--- ark_heroku/views.py
+++ ark_heroku/views.py
@@ -12,40 +12,31 @@ def index(request):
     # トップページ(galary,news,member)
     content = {
     }
-    return render(request, 'ark_heroku/index.html', content)
+    return render(request, 'index.html', content)
 
 
 def gallery(request):
-    posts = Post.objects.order_by('-pub_date')
-    content = {'posts': posts}
-    return render(request, 'ark_heroku/gallery.html', content)
+    content = {}
+    return render(request, 'gallery.html', content)
 
 
 def about(request, id):
-    work = Post.objects.get(id=id)
     content = {
-        'title': work.title,
-        'context': work.body,
-        'date': work.pub_date,
-        'user': work.user,
-        'image': work.image,
+
     }
-    return render(request, 'ark_heroku/about.html', content)
+    return render(request, 'about.html', content)
 
 
 def news(request):
-    newss = News.objects.order_by('-pub_date')
-    content = {'newss': newss}
-    return render(request, 'ark_heroku/news.html', content)
+    content = {}
+    return render(request, 'news.html', content)
 
 
 def idea(request):
-    ideas = News.objects.order_by('-pub_date')
-    content = {'ideas': ideas}
-    return render(request, 'ark_heroku/idea.html', content)
+    content = {}
+    return render(request, 'idea.html', content)
 
 
 def progress(request):
-    idea = Idea.objects.order_by('-pub_date')
-    content = {'idea': idea}
-    return render(request, 'ark_heroku/progress.html', content)
+    content = {}
+    return render(request, 'progress.html', content)
